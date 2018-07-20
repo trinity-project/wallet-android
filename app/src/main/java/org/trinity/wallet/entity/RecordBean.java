483a4f0c0591b9ce75abddb441b2495532167ecc
Yubing <865552569@qq.com>
2018-07-20 15:19:42 +0800

    Improve record bean.

--- app/src/main/java/org/trinity/wallet/entity/RecordBean.java
+++ app/src/main/java/org/trinity/wallet/entity/RecordBean.java
@@ -3,32 +3,42 @@ package org.trinity.wallet.entity;
 import android.support.annotation.NonNull;
 
 public class RecordBean {
-    private ChannelBean channelBean;
-    private String channel_alias;
+    private String channel_Name;
+    private String channel_Alias;
+    private String assetName;
     private double price;
     private double fee;
 
     public RecordBean(@NonNull ChannelBean channelBean, double price, double fee) {
-        this.channelBean = channelBean;
-        this.channel_alias = channelBean.getAlias();
+        this.channel_Name = channelBean.getName();
+        this.channel_Alias = channelBean.getAlias();
+        this.assetName = channelBean.getAssetName();
         this.price = price;
         this.fee = fee;
     }
 
-    public ChannelBean getChannelBean() {
-        return channelBean;
+    public String getChannel_Name() {
+        return channel_Name;
     }
 
-    public void setChannelBean(ChannelBean channelBean) {
-        this.channelBean = channelBean;
+    public void setChannel_Name(String channel_Name) {
+        this.channel_Name = channel_Name;
     }
 
-    public String getChannel_alias() {
-        return channel_alias;
+    public String getChannel_Alias() {
+        return channel_Alias;
     }
 
-    public void setChannel_alias(String channel_alias) {
-        this.channel_alias = channel_alias;
+    public void setChannel_Alias(String channel_Alias) {
+        this.channel_Alias = channel_Alias;
+    }
+
+    public String getAssetName() {
+        return assetName;
+    }
+
+    public void setAssetName(String assetName) {
+        this.assetName = assetName;
     }
 
     public double getPrice() {
